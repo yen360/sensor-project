ff1716757db8a7ca5ef4042ddf399ed785c544c6
Steven <18841105+yen360@users.noreply.github.com>
2021-09-22 09:47:25 +0800

    code for server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/temperature',function(req,res){
+    res.send('23 degC');
+});
+
+app.get('/humidity',function(req,res){
+    res.send('48%');
+});
+
+app.listen(3000,function(){
+    console.log('Server listening on port 3000');
+});
+
+
