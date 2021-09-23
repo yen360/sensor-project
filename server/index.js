70dd2834d44aae3ce5be94b94869d416a4de9c79
Steven <18841105+yen360@users.noreply.github.com>
2021-09-23 16:50:18 +0800

    Update

--- server/index.js
+++ server/index.js
@@ -9,11 +9,19 @@ We now utilize the synchronous methods exported from the 'get-cached-sensor-read
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.get('/temperature', function (req, res) {
-  res.send(getCachedSensorReadings.getTemperature() + '°C')
+  /**
+   * The express response object comes with a built in `json` method
+   * This automatically converts its first argument into a JSON string, and sends it along with the content type headers as a response.
+   */
+  res.json({
+    value: getCachedSensorReadings.getTemperature()
+  })
 })
 
 app.get('/humidity', function (req, res) {
-  res.send(getCachedSensorReadings.getHumidity() + '%')
+  res.json({
+    value: getCachedSensorReadings.getHumidity()
+  })
 })
 
 app.listen(3000, function () {
--- server/public/script.js
+++ server/public/script.js
@@ -1,28 +1,98 @@
-/*
-We put the code for fetching tmeperature in its own function
-*/
+const temperatureCanvasCtx = document.getElementById('temperature-chart').getContext('2d')
+
+const temperatureChartConfig = {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      data: [],
+      backgroundColor: 'rgba(255, 205, 210, 0.5)'
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          suggestedMin: 10,
+          suggestedMax: 40
+        }
+      }]
+    }
+  }
+}
+const temperatureChart = new Chart(temperatureCanvasCtx, temperatureChartConfig)
+
+const humidityCanvasCtx = document.getElementById('humidity-chart').getContext('2d')
+
+const humidityChartConfig = {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      data: [],
+      backgroundColor: 'rgba(197, 202, 233, 0.5)'
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          suggestedMin: 30,
+          suggestedMax: 90
+        }
+      }]
+    }
+  }
+}
+const humidityChart = new Chart(humidityCanvasCtx, humidityChartConfig)
+
+const pushData = (arr, value, maxLen) => {
+  arr.push(value)
+  if (arr.length > maxLen) {
+    arr.shift()
+  }
+}
+
+const humidityDisplay = document.getElementById('humidity-display')
+const temperatureDisplay = document.getElementById('temperature-display')
+
 const fetchTemperature = () => {
   fetch('/temperature')
     .then(results => {
-      return results.text()
+      return results.json()
     })
-    .then(text => {
-      const temperatureDisplay = document.getElementById('temperature-display')
-      temperatureDisplay.innerHTML = text
+    .then(data => {
+      const now = new Date()
+      const timeNow = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
+      pushData(temperatureChartConfig.data.labels, timeNow, 10)
+      pushData(temperatureChartConfig.data.datasets[0].data, data.value, 10)
+      temperatureChart.update()
+      temperatureDisplay.innerHTML = '<strong>' + data.value + '</strong>'
     })
 }
 
-/*
-Make a similar function to fetch humidity
-*/
 const fetchHumidity = () => {
   fetch('/humidity')
     .then(results => {
-      return results.text()
+      return results.json()
     })
-    .then(text => {
-      const temperatureDisplay = document.getElementById('humidity-display')
-      temperatureDisplay.innerHTML = text
+    .then(data => {
+      const now = new Date()
+      const timeNow = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
+      pushData(humidityChartConfig.data.labels, timeNow, 10)
+      pushData(humidityChartConfig.data.datasets[0].data, data.value, 10)
+      humidityChart.update()
+      humidityDisplay.innerHTML = '<strong>' + data.value + '</strong>'
     })
 }
 
