d7cff5acedccfdfa729fe914bc45c35d217cd087
Steven <18841105+yen360@users.noreply.github.com>
2021-09-23 15:51:09 +0800

    Update

--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ app.get('/humidity', function (req, res) {
   res.send(getCachedSensorReadings.getHumidity() + '%')
 })
 
+app.get('/public', function (req, res) {
+  res.sendFile(path.join(__dirname,'index.html'))
+})
+
 app.listen(3000, function () {
   console.log('Server listening on port 3000')
 })
